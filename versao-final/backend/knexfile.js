0217ede6404148f86a8ac58063ec4f4ad250730d
Brendon Henrique <brendon.henrrique@hotmail.com>
2023-07-18 17:24:36 -0300

    Projeto  Base de Conhecimento Completo

---
+++ versao-final/backend/knexfile.js
@@ -0,0 +1,18 @@
+const { db } = require('./.env')
+
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = { 
+    client: 'postgresql',
+    connection: db ,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+};
