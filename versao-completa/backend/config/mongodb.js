edd5e337700679b82bac22a3f114e9537cf8b8e5
Brendon Henrique <brendon.henrrique@hotmail.com>
2023-08-01 07:22:06 -0300

    atualizando projeto para deploy

--- versao-completa/backend/config/mongodb.js
+++ versao-completa/backend/config/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/knowledge_stats', { useNewUrlParser: true })
+mongoose.connect('mongodb+srv://base-de-conhecimento:qOr6UkC5uTgTsScx@basedeconhecimento.fljy2un.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true })
     .catch(e => {
         const msg = 'ERRO! Não foi possível conectar com o MongoDB!'
         console.log('\x1b[41m%s\x1b[37m', msg, '\x1b[0m')
