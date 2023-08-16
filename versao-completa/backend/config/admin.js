ea4df10dd1eacea7ca2a556d742e04d9d48361ce
Brendon Henrique <brendon.henrrique@hotmail.com>
2023-08-16 09:19:40 -0300

    autenticação do administrador

--- versao-completa/backend/config/admin.js
+++ versao-completa/backend/config/admin.js
@@ -1,9 +1,7 @@
-module.exports = middleware => {
-    return (req, res, next) => {
-        if(req.user.admin) {
-            middleware(req, res, next)
-        } else {
-            res.status(401).send('Usuário não é administrador.')
-        }
+module.exports = (req, res, next) => {
+    if (req.user.isAdmin) { 
+        next(); 
+    } else {
+        res.status(403).send('Acesso negado.'); 
     }
-}
+};
--- versao-completa/backend/config/routes.js
+++ versao-completa/backend/config/routes.js
@@ -7,15 +7,15 @@ module.exports = app => {
     app.post('/validateAdmin', app.api.auth.validateAdmin)
 
     app.route('/users')
-        .all(app.config.passport.authenticate())
-        .post(admin(app.api.user.save))
-        .get(admin(app.api.user.get))
+    .all(app.config.passport.authenticate())
+    .post(admin, app.api.user.save) 
+    .get(admin, app.api.user.get)   
 
     app.route('/users/:id')
-        .all(app.config.passport.authenticate())
-        .put(admin(app.api.user.save))
-        .get(admin(app.api.user.getById))
-        .delete(admin(app.api.user.remove))
+    .all(app.config.passport.authenticate())
+    .put(admin, app.api.user.save)   
+    .get(admin, app.api.user.getById) 
+    .delete(admin, app.api.user.remove) 
 
     app.route('/categories')
         .all(app.config.passport.authenticate())
